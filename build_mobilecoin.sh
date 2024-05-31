66ed08f0da35c9157ba270597d5a83eed809a69a
Henry Holtzman <henry@mobilecoin.com>
2024-05-31 13:35:48 -0700

    build_mobilecoin.sh updated to do a linux-only build when run on linux

--- build_mobilecoin.sh
+++ build_mobilecoin.sh
@@ -3,14 +3,12 @@
 # Build libmobilecoin.a and *.h artifacts for mobilecoin-go's include subdir
 #
 # prerequesites:
-# 1. run on aarch MacOS (may also work on x86 MacOS)
-#    a. will cross compile for x86 MacOS
-#    b. will cross compile for linux
-#    c. user may choose instead to build natively on 3 machines and combine the results
-# 2. Recent Xcode is installed (15.3 as of this writing)
-# 3. libmobilecoin.git is cloned in the same parent directory as mobilecoin-go (../libmobilecoin)
+# 1. run on MacOS
+#    a. will cross compile for other flavor (x86/aarch) of MacOS and x86 Linux
+#    b. can also run on Linux, but will only build for x86 Linux
+# 2. libmobilecoin.git should be cloned and in the same parent directory as mobilecoin-go (../libmobilecoin)
 #     cd ..; git clone https://github.com/mobilecoinofficial/libmobilecoin.git --recurse-submodules
-#
+# 3.  The protobuf compiler should already be installed
 
 cd ../libmobilecoin/libmobilecoin
 
@@ -20,35 +18,55 @@ export SGX_MODE=HW
 # (as well as current testnet, which is already upgraded to DCAP)
 export IAS_MODE=PROD
 
-# rust current builds on MacOS targeting MacOS 11.0 and later.
-# force this build to be consistent to avoid ld warnings from go.
-export MACOSX_DEPLOYMENT_TARGET=11.0
+OS=$(uname -s)
 
-# build for MacOS on x86 and aarch64; and, linux on x86
-declare -a TARGETS=(x86_64-apple-darwin aarch64-apple-darwin x86_64-unknown-linux-gnu)
+case ${OS} in
+    Darwin)
+        # MacOS
+        # build for MacOS on x86 and aarch64; and, linux on x86
+        declare -a TARGETS=(x86_64-apple-darwin aarch64-apple-darwin x86_64-unknown-linux-gnu)
+
+        # rust current builds on MacOS targeting MacOS 11.0 and later.
+        # force this build to be consistent to avoid ld warnings from go.
+        export MACOSX_DEPLOYMENT_TARGET=11.0
+
+        # make sure linux cross compiling tools are installed
+        brew tap SergioBenitez/osxct
+        brew install x86_64-unknown-linux-gnu
+        export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER=x86_64-unknown-linux-gnu-gcc
+        ;;
+    Linux)
+        # Linux
+        declare -a TARGETS=(x86_64-unknown-linux-gnu)
+        ;;
+    *)
+        echo "Unsupported OS"
+        exit 1
+        ;;
+esac
 
 # make sure rust support for all of our target platforms is installed
 for t in ${TARGETS[@]}; do
     rustup target add $t
 done
 
-# make sure linux cross compiling tools are installed
-brew tap SergioBenitez/osxct
-brew install x86_64-unknown-linux-gnu
-export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER=x86_64-unknown-linux-gnu-gcc
-
 # build the libraries
 for t in ${TARGETS[@]}; do
     cargo build --release --target $t
 done
 
-# combine to a universal MacOS library
-lipo -create target/aarch64-apple-darwin/release/libmobilecoin.a target/x86_64-apple-darwin/release/libmobilecoin.a -output target/release/libmobilecoin.a
+case ${OS} in
+    Darwin)
+    # combine to a universal MacOS library
+    lipo -create target/aarch64-apple-darwin/release/libmobilecoin.a target/x86_64-apple-darwin/release/libmobilecoin.a -output target/release/libmobilecoin.a
+    # copy the universal MacOS library into mobilecoin-go's include subdir
+    cp target/release/libmobilecoin.a ../../mobilecoin-go/include
+    ;;
+esac
 
 # copy and rename the linux library
 cp target/x86_64-unknown-linux-gnu/release/libmobilecoin.a target/release/libmobilecoin_linux.a
 
-# copy everything in into mobilecoin-go's include subdir
+# copy headers and linux library in into mobilecoin-go's include subdir
 cp include/* ../../mobilecoin-go/include
-cp target/release/libmobilecoin.a ../../mobilecoin-go/include
 cp target/release/libmobilecoin_linux.a ../../mobilecoin-go/include
